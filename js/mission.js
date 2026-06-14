3dcc373bd4b0dd0ae30889f51a1baa9b202278d4
theodore-404 <tioonlygame@gmail.com>
2026-06-14 17:40:10 +0700

    Create mission.js

---
+++ js/mission.js
@@ -0,0 +1,53 @@
+import {
+    getData,
+    saveData
+}
+from "./storage.js";
+
+export function renderMissions() {
+    const missions =
+    getData("missions");
+    const missionList =
+    document.getElementById("missionList");
+    missionList.innerHTML = "";
+    missions.forEach(mission => {
+        const li =
+        document.createElement("li");
+        li.className =
+        "flex justify-between border border-green-700 p-2";
+        li.innerHTML = `
+        <span>${mission.title}</span>
+        <button
+        class="delete-btn"
+        data-id="${mission.id}">X</button>`;
+        missionList.appendChild(li);
+    });
+}
+
+export function addMission() {
+    const input =
+    document.getElementById("missionInput");
+    const title =
+    input.value.trim();
+    if (!title) return;
+    const missions =
+    getData("missions");
+    missions.push({
+        id: Date.now(),
+        title: title,
+        completed: false
+    });
+
+    saveData("missions", missions);
+    input.value = "";
+    renderMissions();
+}
+
+export function deleteMission(id) {
+    const missions =
+    getData("missions");
+    const filtered =
+    missions.filter(mission => mission.id != id);
+    saveData("missions", filtered);
+    renderMissions();
+}
